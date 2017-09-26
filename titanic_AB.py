25342b7abc82d98d07d7d8c76d5c1a29a762a93d
abakir <ahmed.m.bakir@gmail.com>
2017-09-26 20:06:46 +0200

    Add files via upload

--- titanic_AB.py
+++ titanic_AB.py
@@ -1,6 +1,5 @@
 
 
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -8,18 +7,24 @@ from sklearn.preprocessing import MinMaxScaler
 train1 = pd.read_csv('train.csv')
 train2 = train1.fillna(0)
 
-train3 = train2.drop(['Name','Sex','Ticket', 'Cabin', 'Embarked'],1)
+train3 = train2.drop(['PassengerId','Name','Sex','Ticket', 'Cabin', 'Embarked'],1)
 train = pd.DataFrame(train3).astype(float)
 
 test1 = pd.read_csv('test.csv')
 test2 = test1.fillna(0)
 
-test3 = test2.drop(['Name','Sex','Ticket', 'Cabin', 'Embarked'], 1)
+test3 = test2.drop(['PassengerId','Name','Sex','Ticket', 'Cabin', 'Embarked'], 1)
 
 
 scaler = MinMaxScaler()
 
 
-train_scaled = pd.DataFrame(scaler.fit_transform(train))
+train_scaled = pd.DataFrame(scaler.fit_transform(train), columns=['Survived','Pclass','Age','SibSp','Parch','Fare'])
 
 print(train_scaled.head(10))
+
+#Feature Selection
+
+# Check for correlation among variables and with target
+
+print(train_scaled.corr())
