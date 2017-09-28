811fa69cf2465e0ec92a89a41572d0aaa794cbf0
Sai Sree Kamineni <ksaisree849@gmail.com>
2017-09-27 22:38:40 -0700

    updated age_trans function

--- titanic_AB.py
+++ titanic_AB.py
@@ -6,31 +6,30 @@ from sklearn.preprocessing import MinMaxScaler
 
 train1 = pd.read_csv('train.csv').fillna(0)
 
-
-def age_trans(data):
-    Age = [float(age) for age in data['Age']]
-    for i in Age:
+def age_trans(i):
         if i < 13:
-            data.replace(i,1, inplace=True)
+            return 1
         elif i >= 13 and i < 18:
-            data.replace(i,2, inplace=True)
+            return 2
         elif i >= 18 and i < 25:
-            data.replace(i,3, inplace=True)
+            return 3
         elif i >= 25 and i < 35:
-            data.replace(i,4, inplace=True)
+            return 4
         elif i >= 35 and i < 46:
-            data.replace(i,5, inplace=True)
+            return 4
         elif i >= 46 and i < 56:
-            data.replace(i,6, inplace=True)
+            return 6
         elif i >= 56 and i < 66:
-            data.replace(i,7, inplace=True)
+            return 7
         else:
-            data.replace(i,8, inplace=True)
-    return data
+            return 8
 
-age_train = age_trans(train1)
-print(age_train.head(50))
-print(train1['Age'].describe())
+        
+train1['Age'] = train1['Age'].apply(lambda x: age_trans(x))
+
+#age_train = age_trans(train1)
+#print(age_train.head(50))
+#print(train1['Age'].describe())
 
 #
 # train3 = train2.drop(['PassengerId','Name','Sex','Ticket', 'Cabin', 'Embarked'],1)
